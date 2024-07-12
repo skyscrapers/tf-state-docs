d7f1afac67dc091d36a8ea90a3b288c9e2cecd16
nolan <nolan@skyscrapers.eu>
2024-07-12 14:19:36 +0200

    adding error print

--- gen-wiki.py
+++ gen-wiki.py
@@ -62,6 +62,7 @@ def run_terragrunt(directory):
         print(f"Directory = ", directory)
         return result.stdout
     except subprocess.CalledProcessError as e:
+        print(f"Result", result.stdout)
         print(f"Error: ", e)
         return None
 
